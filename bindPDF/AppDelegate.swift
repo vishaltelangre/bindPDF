1f77a43e00555aa39c5a77a1a2518e7dbb6cbc9e
Vishal Telangre <the@vishaltelangre.com>
2019-10-13 20:42:23 +0530

    Combine multiple PDFs in a single PDF file

---
+++ bindPDF/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  bindPDF
+//
+//  Created by Vishal M Telangre on 10/12/19.
+//  Copyright © 2019 Vishal Telangre. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
